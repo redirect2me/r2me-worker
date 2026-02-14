d5963c40b3105dc53ec010c73251b2809f41db30
Andrew Marcuse <fileformat@gmail.com>
2026-02-14 14:32:13 -0500

    Github releases

--- pkg/nfpm/postremove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# postremove script for redirect2me package
-#
-
-# Only run on 'remove' (not upgrade) to avoid deleting user during updates
-if [ "$1" = "remove" ] || [ "$1" = "purge" ]; then
-    userdel redirect2me || true
-fi
